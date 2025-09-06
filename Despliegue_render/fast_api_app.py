5aa7beaaf37b09b3d5efabf4e8a4a24c8eb1cc7d
FernandoMoroC <fernandomorocordobes@gmail.com>
2025-09-06 08:54:01 +0200

    fastapi

--- Despliegue_render/fast_api_app.py
+++ Despliegue_render/fast_api_app.py
@@ -4,24 +4,6 @@ import utils
 import pandas
 import os
 from pydantic import BaseModel
-
-
-# Creamos una clase para los valores de las variables:
-
-class DatosReclamacion(BaseModel):
-    Complaint_ID:int
-    Product:str
-    Sub_product: str
-    Issue: str
-    Sub_issue: str
-    State: str
-    ZIP_code: int
-    Date_received: str
-    Date_sent_to_company: str
-    Company: str
-
-
-
 from fastapi import FastAPI
 
 app = FastAPI(
@@ -35,12 +17,19 @@ modelo = joblib.load(modelo_path) # carga todo el modelo
 
 @app.post("/predict")
 
-def predecir_valores(reclamacion: DatosReclamacion):
-    df = pd.DataFrame([{'Complaint ID': reclamacion.Complaint_ID , 'Product': reclamacion.Product, 
-                        'Sub-product': reclamacion.Sub_product, 'Issue': reclamacion.Issue, 'Sub-issue': reclamacion.Sub_issue,
-                        'State': reclamacion.State,'ZIP code': reclamacion.ZIP_code, 'Date received': reclamacion.Date_received,
-                         'Date sent to company': reclamacion.Date_sent_to_company, 'Company': reclamacion.Company
-    }])
+def predecir_valores(Complaint_ID:int, Product:str, Sub_product: str, Issue: str,  Sub_issue: str,
+                      State: str, ZIP_code: int, Date_received: str, Date_sent_to_company: str, Company: str):
+    df = pd.DataFrame([{'Complaint ID': Complaint_ID ,
+                         'Product': Product, 
+                        'Sub-product': Sub_product,
+                        'Issue': Issue,
+                        'Sub-issue': Sub_issue,
+                        'State': State,
+                        'ZIP code': ZIP_code,
+                        'Date received': Date_received,
+                        'Date sent to company': Date_sent_to_company,
+                        'Company': Company}])
+    
     pred = modelo.predict(df)
     prob = modelo.predict_proba(df)
 
